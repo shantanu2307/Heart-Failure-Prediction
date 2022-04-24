d7e86f3e9e8bf1472eabae6ccdd7bf3295c83263
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2022-04-25 00:46:09 +0530

    Update

--- unit_test.py
+++ unit_test.py
@@ -12,6 +12,7 @@ data={
 
 def test_index_route():
     response = app.test_client().get('/')
+    print("All Good!")
     assert response.status_code == 200
 
 @app.route("/predict_api")
@@ -19,6 +20,8 @@ def test_api():
     """ function to test the model """
     response=app.test_client().post('/predict_api',json=json.dumps(data))
     response_data=json.loads(response.data)
+    print(data)
+    print(response_data)
     assert response.status_code == 200
     assert response_data == {'output': '1'}
 
